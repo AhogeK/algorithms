1fa8d22bc9cc3ced269c147830c26b241bacd87e
AhogeK <ahogek@gmail.com>
2023-02-28 12:50:46 +0800

    feat(排序): 选择排序复习

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -11,7 +11,7 @@ public class SelectionSort {
     public int[] sortArray(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int minIndex = i;
-            for (int j = i; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] < nums[minIndex]) {
                     minIndex = j;
                 }
