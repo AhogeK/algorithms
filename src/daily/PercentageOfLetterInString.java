fe339eaf5dace47342160fcf7b2654d521a8f01b
AhogeK <ahogek@gmail.com>
2025-04-01 15:44:42 +0800

    feat(daily): 添加两个新的每日算法题解
    - 新增 PercentageOfLetterInString 类，用于计算字母在字符串中的百分比
    - 新增 SolvingQuestionsWithBrainpower 类，用于解决智力问题

---
+++ src/daily/PercentageOfLetterInString.java
@@ -0,0 +1,25 @@
+package daily;
+
+/**
+ * <a href="https://leetcode.cn/problems/percentage-of-letter-in-string">2278. 字母在字符串中的百分比</a>
+ *
+ * @author AhogeK
+ * @since 2025-03-31 07:50:38
+ */
+public class PercentageOfLetterInString {
+
+    public int percentageLetter(String s, char letter) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == letter) {
+                count++;
+            }
+        }
+        return count * 100 / s.length();
+    }
+
+    public static void main(String[] args) {
+        PercentageOfLetterInString percentageOfLetterInString = new PercentageOfLetterInString();
+        System.out.println(percentageOfLetterInString.percentageLetter("foobar", 'o'));
+    }
+}
