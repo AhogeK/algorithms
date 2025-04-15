b60542a806f7ca394098c32b2bb7c4e90de58c9f
AhogeK <ahogek@gmail.com>
2025-04-15 12:32:10 +0800

    feat(ListNode): 添加数组构造函数- 为 ListNode 类添加了一个新的构造函数，可以接收一个整数数组
    - 新增的构造函数能够将数组元素转换为链表节点
    - 优化了链表的创建过程，提高了代码的可读性和实用性

--- src/common/ListNode.java
+++ src/common/ListNode.java
@@ -20,6 +20,17 @@ public class ListNode {
         this.next = next;
     }
 
+    public ListNode(int[] arr) {
+        if (arr == null || arr.length == 0) throw new IllegalArgumentException("arr can not be empty");
+
+        this.val = arr[0];
+        ListNode curr = this;
+        for (int i = 1; i < arr.length; i++) {
+            curr.next = new ListNode(arr[i]);
+            curr = curr.next;
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
