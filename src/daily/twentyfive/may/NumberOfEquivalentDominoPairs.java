410f294c757a82656013313b50f2212efbf0c10a
AhogeK <ahogek@gmail.com>
2025-05-04 14:10:16 +0800

    feat(daily): 添加 1128. 等价多米诺骨牌对的数量
    - 新增 NumberOfEquivalentDominoPairs 类实现等价多米诺骨牌对数量的计算
    - 采用计数数组和编码方式高效解决问题
    - 代码结构清晰，注释简洁明了

---
+++ src/daily/twentyfive/may/NumberOfEquivalentDominoPairs.java
@@ -0,0 +1,23 @@
+package daily.twentyfive.may;
+
+/**
+ * 1128. 等价多米诺骨牌对的数量
+ *
+ * @author AhogeK
+ * @since 2025-05-04 14:02:35
+ */
+public class NumberOfEquivalentDominoPairs {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] cnt = new int[100];
+        int ans = 0;
+        for (int[] d : dominoes) {
+            int a = d[0];
+            int b = d[1];
+            // 想当于 min(a, b) * 10 + max(a, b)
+            int code = a < b ? a * 10 + b : b * 10 + a;
+            ans += cnt[code];
+            cnt[code]++;
+        }
+        return ans;
+    }
+}
