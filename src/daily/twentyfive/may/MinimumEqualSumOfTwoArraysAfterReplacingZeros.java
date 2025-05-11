40199ee713c3f93c5580e0e3ec557d57ce718567
AhogeK <ahogek@gmail.com>
2025-05-12 00:26:21 +0800

    feat(daily): 添加 2918. 数组的最小相等和
    - 新增 MinimumEqualSumOfTwoArraysAfterReplacingZeros 类
    - 实现 minSum 方法，计算两个数组的最小相等和
    - 处理特殊情况，当一个数组中没有零且和大于另一个数组时返回 -1

---
+++ src/daily/twentyfive/may/MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
@@ -0,0 +1,35 @@
+package daily.twentyfive.may;
+
+/**
+ * 2918. 数组的最小相等和
+ *
+ * @author AhogeK
+ * @since 2025-05-11 23:21:18
+ */
+public class MinimumEqualSumOfTwoArraysAfterReplacingZeros {
+
+    public long minSum(int[] nums1, int[] nums2) {
+        long s1 = 0;
+        long s2 = 0;
+        int c1 = 0;
+        int c2 = 0;
+
+        for (int v : nums1) {
+            if (v == 0) c1++;
+            else s1 += v;
+        }
+
+        for (int v : nums2) {
+            if (v == 0) c2++;
+            else s2 += v;
+        }
+
+        s1 += c1;
+        s2 += c2;
+
+        if ((c1 == 0 && s2 > s1) || (c2 == 0 && s1 > s2))
+            return -1;
+
+        return Math.max(s1, s2);
+    }
+}
