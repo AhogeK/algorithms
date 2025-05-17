f262ddc8ceb73514b855eb433d21657a69a2db72
AhogeK <ahogek@gmail.com>
2025-05-17 15:33:55 +0800

    feat(daily): 添加颜色分类问题的解决方案
    - 实现了颜色分类问题的解决方案，将数组中的 0、1、2按升序排序
    - 采用双指针法，提高排序效率

---
+++ src/daily/twentyfive/may/SortColors.java
@@ -0,0 +1,30 @@
+package daily.twentyfive.may;
+
+/**
+ * 75. 颜色分类
+ *
+ * @author AhogeK
+ * @since 2025-05-17 15:31:15
+ */
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, i = 0, p2 = n - 1;
+        while (i <= p2) {
+            if (nums[i] == 0) {
+                int tmp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = tmp;
+                p0++;
+                i++;
+            } else if (nums[i] == 2) {
+                int tmp = nums[i];
+                nums[i] = nums[p2];
+                nums[p2] = tmp;
+                p2--;
+            } else {
+                i++;
+            }
+        }
+    }
+}
