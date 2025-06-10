18543e3b64ca8b27f16b1ede4cd215af117e4242
AhogeK <ahogek@gmail.com>
2025-06-10 19:20:24 +0800

    feat(daily): 添加奇偶频次最大差值 I 功能
    - 实现了计算字符串中奇偶频次字符的最大差值的功能
    - 新增 MaximumDifferenceBetweenEvenAndOddFrequencyI 类和 maxDifference 方法
    - 使用数组和排序来统计和计算字符频次

---
+++ src/daily/twentyfive/jun/MaximumDifferenceBetweenEvenAndOddFrequencyI.java
@@ -0,0 +1,31 @@
+package daily.twentyfive.jun;
+
+import java.util.Arrays;
+
+/**
+ * 3442. 奇偶频次间的最大差值 I
+ *
+ * @author AhogeK
+ * @since 2025-06-10 19:18:57
+ */
+public class MaximumDifferenceBetweenEvenAndOddFrequencyI {
+
+    public int maxDifference(String s) {
+        int[] chars = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i) - 'a']++;
+        }
+        int doubles = 1000;
+        int single = -1000;
+        Arrays.sort(chars);
+        for (int i = 0; i < 26; i++) {
+            if (chars[i] != 0) {
+                if (chars[i] % 2 == 1)
+                    single = Math.max(chars[i], single);
+                else
+                    doubles = Math.min(chars[i], doubles);
+            }
+        }
+        return single - doubles;
+    }
+}
