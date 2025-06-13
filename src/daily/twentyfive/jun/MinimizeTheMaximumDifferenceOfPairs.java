fe8b6ad35a8654254a6ae0147f668bd1e55a4d74
AhogeK <ahogek@gmail.com>
2025-06-13 19:27:16 +0800

    feat(daily): 添加 2616. 最小化数对的最大差值问题的解决方案
    - 新增 MinimizeTheMaximumDifferenceOfPairs 类
    - 实现了 minimizeMax 方法来解决该问题
    - 使用二分查找和贪心算法来找到最小化数对最大差值的解决方案

---
+++ src/daily/twentyfive/jun/MinimizeTheMaximumDifferenceOfPairs.java
@@ -0,0 +1,37 @@
+package daily.twentyfive.jun;
+
+import java.util.Arrays;
+
+/**
+ * 2616. 最小化数对的最大差值
+ *
+ * @author AhogeK
+ * @since 2025-06-13 19:26:40
+ */
+public class MinimizeTheMaximumDifferenceOfPairs {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums[nums.length - 1] - nums[0];
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int count = 0;
+            for (int i = 0; i < nums.length - 1; ) {
+                if (nums[i + 1] - nums[i] <= mid) {
+                    count++;
+                    i += 2;
+                } else {
+                    i++;
+                }
+            }
+            if (count >= p) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
