ab422fd4e3b63002e228dbf5881a42ba9f454581
AhogeK <ahogek@gmail.com>
2025-06-22 18:32:20 +0800

    feat(daily): 添加将字符串拆分为若干长度为 k 的组的功能
    - 新增 DivideAStringIntoGroupsOfSizeK 类
    - 实现 divideString 方法，将字符串 s 按照长度 k 进行拆分
    - 如果最后一个组长度不足 k，则使用指定字符 fill 进行填充
    - 返回拆分后的字符串数组

---
+++ src/daily/twentyfive/jun/DivideAStringIntoGroupsOfSizeK.java
@@ -0,0 +1,26 @@
+package daily.twentyfive.jun;
+
+/**
+ * 2138. 将字符串拆分为若干长度为 k 的组
+ *
+ * @author AhogeK
+ * @since 2025-06-22 18:16:49
+ */
+public class DivideAStringIntoGroupsOfSizeK {
+    public String[] divideString(String s, int k, char fill) {
+        int n = s.length();
+        String[] ans = new String[(n + k - 1) / k];
+        int curr = 0;
+        while (curr < n) {
+            int end = Math.min(curr + k, n);
+            ans[curr / k] = s.substring(curr, end);
+            curr += k;
+        }
+        String last = ans[ans.length - 1];
+        if (last.length() < k) {
+            last += String.valueOf(fill).repeat(k - last.length());
+            ans[ans.length - 1] = last;
+        }
+        return ans;
+    }
+}
