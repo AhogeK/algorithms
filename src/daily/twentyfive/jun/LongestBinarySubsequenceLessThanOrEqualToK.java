bc6dc47b32fdc22f4f34caef24468af648f315d2
AhogeK <ahogek@gmail.com>
2025-06-26 23:14:27 +0800

    feat(daily): 添加小于等于 K 的最长二进制子序列问题解决方案
    - 新增 LongestBinarySubsequenceLessThanOrEqualToK 类
    - 实现 longestSubsequence 方法，解决给定字符串 s 和整数 k 的最长二进制子序列问题
    - 算法思路：
      - 统计字符串中的0 的数量，作为初始最长子序列长度
      - 从字符串末尾开始遍历，处理每个 1
      - 使用位运算计算当前值，确保不超过 k  - 动态更新最长子序列长度

---
+++ src/daily/twentyfive/jun/LongestBinarySubsequenceLessThanOrEqualToK.java
@@ -0,0 +1,36 @@
+package daily.twentyfive.jun;
+
+/**
+ * 2311. 小于等于 K 的最长二进制子序列
+ *
+ * @author AhogeK
+ * @since 2025-06-26 23:08:42
+ */
+public class LongestBinarySubsequenceLessThanOrEqualToK {
+    public int longestSubsequence(String s, int k) {
+        int len = s.length();
+        int zeroCnt = 0;
+        for (int i = 0; i < len; i++)
+            if (s.charAt(i) == '0')
+                zeroCnt++;
+        int ans = zeroCnt;
+        long value = 0;
+        int power = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            if (s.charAt(i) == '1') {
+                if (power < 32) {
+                    long cur = 1L << power;
+                    if (value + cur <= k) {
+                        value += cur;
+                        ans++;
+                    }
+                }
+                power++;
+            } else {
+                power++;
+            }
+            if ((1L << power) > k) break;
+        }
+        return ans;
+    }
+}
