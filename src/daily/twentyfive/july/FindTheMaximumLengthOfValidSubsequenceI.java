e44205cc6621391fd7485383fa4e77219606749a
AhogeK <ahogek@gmail.com>
2025-07-16 17:31:26 +0800

    feat(daily): 添加找出有效子序列的最大长度 I 功能
    - 实现了找出有效子序列的最大长度 I 功能
    - 新增 FindTheMaximumLengthOfValidSubsequenceI 类
    - 使用动态规划方法解决该问题

---
+++ src/daily/twentyfive/july/FindTheMaximumLengthOfValidSubsequenceI.java
@@ -0,0 +1,19 @@
+package daily.twentyfive.july;
+
+/**
+ * 3201. 找出有效子序列的最大长度 I
+ *
+ * @author AhogeK
+ * @since 2025-07-16 17:23:00
+ */
+public class FindTheMaximumLengthOfValidSubsequenceI {
+    public int maximumLength(int[] nums) {
+        int[][] maxLength = new int[2][2];
+        for (int num : nums) {
+            int parity = num % 2;
+            maxLength[parity][0]++;
+            maxLength[parity][1] = maxLength[1 - parity][1] + 1;
+        }
+        return Math.max(Math.max(maxLength[0][0], maxLength[0][1]), Math.max(maxLength[1][0], maxLength[1][1]));
+    }
+}
