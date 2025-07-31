9e37dd95bb6211da205783045f44a081435ddc76
AhogeK <ahogek@gmail.com>
2025-07-31 16:46:52 +0800

    feat(daily): add solution for Neighboring Bitwise Xor problem
    - Implement NeighboringBitwiseXor class with a method to check if a valid array exists for the given derived array
    - The solution uses bitwise XOR operation to determine the validity of the array

---
+++ src/daily/twentyfive/july/NeighboringBitwiseXor.java
@@ -0,0 +1,16 @@
+package daily.twentyfive.july;
+
+/**
+ * 2683. 相邻值的按位异或
+ *
+ * @author AhogeK
+ * @since 2025-07-31 15:07:08
+ */
+public class NeighboringBitwiseXor {
+    public boolean doesValidArrayExist(int[] derived) {
+        int xor = 0;
+        for (int x : derived)
+            xor ^= x;
+        return xor == 0;
+    }
+}
