c1c13f0703d568e592f281281e1d141cee13c0b4
AhogeK <ahogek@gmail.com>
2025-08-19 09:56:35 +0800

    feat(daily): add solution for problem 2348. 全 0 子数组的数目
    - Create new class NumberOfZeroFilledSubarrays
    - Implement method zeroFilledSubarray to solve the problem
    - Add relevant comments and package declaration

---
+++ src/daily/twentyfive/aug/NumberOfZeroFilledSubarrays.java
@@ -0,0 +1,20 @@
+package daily.twentyfive.aug;
+
+/**
+ * 2348. 全 0 子数组的数目
+ *
+ * @author AhogeK
+ * @since 2025-08-19 09:54:53
+ */
+public class NumberOfZeroFilledSubarrays {
+    public long zeroFilledSubarray(int[] nums) {
+        long res = 0;
+        int n = nums.length;
+        int last = -1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) last = i;
+            res += i - last;
+        }
+        return res;
+    }
+}
