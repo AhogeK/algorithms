efce846515929ca613e9147208b276591d5cf5b4
AhogeK <ahogek@gmail.com>
2025-08-22 16:26:52 +0800

    feat(daily): add solution for LeetCode 3195
    - Implement FindTheMinimumAreaToCoverAllOnesI class
    - Add minimumArea method to calculate the minimum area covering

---
+++ src/daily/twentyfive/aug/FindTheMinimumAreaToCoverAllOnesI.java
@@ -0,0 +1,27 @@
+package daily.twentyfive.aug;
+
+/**
+ * 3195. 包含所有 1 的最小矩形面积 I
+ *
+ * @author AhogeK
+ * @since 2025-08-22 16:24:27
+ */
+public class FindTheMinimumAreaToCoverAllOnesI {
+    public int minimumArea(int[][] grid) {
+        int minR = 1000;
+        int maxR = -1;
+        int minC = 1000;
+        int maxC = -1;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    minR = Math.min(minR, i);
+                    maxR = Math.max(maxR, i);
+                    minC = Math.min(minC, j);
+                    maxC = Math.max(maxC, j);
+                }
+            }
+        }
+        return (maxR - minR + 1) * (maxC - minC + 1);
+    }
+}
