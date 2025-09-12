d69681d061988f2d6e349061823ee16f0ad40453
AhogeK <ahogek@gmail.com>
2025-09-12 16:07:53 +0800

    feat(daily): add solution forVowels Game In A String problem
    - Create VowelsGameInAString class to solve the problem
    - Implement doesAliceWin method to check if Alice wins the game
    - Iterate through the string to find any vowel character
    - Return true if a vowel is found, otherwise false

---
+++ src/daily/twentyfive/sep/VowelsGameInAString.java
@@ -0,0 +1,18 @@
+package daily.twentyfive.sep;
+
+/**
+ * 3227. 字符串元音游戏
+ *
+ * @author AhogeK
+ * @since 2025-09-12 16:03:43
+ */
+public class VowelsGameInAString {
+    public boolean doesAliceWin(String s) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+        }
+        return false;
+    }
+}
