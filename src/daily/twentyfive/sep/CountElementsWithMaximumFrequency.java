c2d390bff932e11e5b0f8410c1f4e792b7f91bd1
AhogeK <ahogek@gmail.com>
2025-09-22 13:42:16 +0800

    feat(daily): add solution for counting elements with maximum frequency
    Implement solution for problem 3005 to count elements that appear with maximum frequency
    in the input array. Uses counting array approach with O(n) time complexity.

---
+++ src/daily/twentyfive/sep/CountElementsWithMaximumFrequency.java
@@ -0,0 +1,23 @@
+package daily.twentyfive.sep;
+
+/**
+ * 3005. 最大频率元素计数
+ *
+ * @author AhogeK
+ * @since 2025-09-22 13:30:17
+ */
+public class CountElementsWithMaximumFrequency {
+    public int maxFrequencyElements(int[] nums) {
+        int[] cnt = new int[101];
+        int maxf = 0;
+        for (int num : nums) {
+            cnt[num]++;
+            if (cnt[num] > maxf) maxf = cnt[num];
+        }
+        int res = 0;
+        for (int c : cnt)
+            if (c == maxf)
+                res += c;
+        return res;
+    }
+}
