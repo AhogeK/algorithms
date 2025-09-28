881c354322628a1ec8e0d543f8350cffb32bc8f6
AhogeK <ahogek@gmail.com>
2025-09-28 19:14:41 +0800

    feat(leetcode): add solution for largest perimeter triangle problem
    - Implement greedy algorithm with sorting
    - Check triangle inequality from largest sides
    - Return maximum valid perimeter or zero
    - Add problem description and author info
    - Include time complexity O(n log n) due to sorting

---
+++ src/daily/twentyfive/sep/LargestPerimeterTriangle.java
@@ -0,0 +1,20 @@
+package daily.twentyfive.sep;
+
+import java.util.Arrays;
+
+/**
+ * 976. 三角形的最大周长
+ *
+ * @author AhogeK
+ * @since 2025-09-28 19:05:24
+ */
+public class LargestPerimeterTriangle {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            int a = nums[i - 2], b = nums[i - 1], c = nums[i];
+            if (a + b > c) return a + b + c;
+        }
+        return 0;
+    }
+}
