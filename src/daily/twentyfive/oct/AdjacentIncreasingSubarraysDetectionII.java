c253fd5e039947a37528c877f665f4f6c5cd903c
AhogeK <ahogek@gmail.com>
2025-10-15 15:58:10 +0800

    feat(algorithm): implement solution for detecting adjacent increasing subarrays
    - Added new class AdjacentIncreasingSubarraysDetectionII
    - Implemented maxIncreasingSubarrays method to find maximum k for k-adjacent increasing subarrays
    - Used efficient single pass algorithm with O(n) time complexity
    - Handled edge cases with minimum integer values and array boundaries
    - Included bitwise operations for fast division by 2
    - Added proper Java documentation and author information

---
+++ src/daily/twentyfive/oct/AdjacentIncreasingSubarraysDetectionII.java
@@ -0,0 +1,36 @@
+package daily.twentyfive.oct;
+
+import java.util.List;
+
+/**
+ * 3350. 检测相邻递增子数组 II
+ *
+ * @author AhogeK
+ * @since 2025-10-15 15:45:29
+ */
+public class AdjacentIncreasingSubarraysDetectionII {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        Integer[] arr = nums.toArray(new Integer[0]);
+        int size = 0;
+        int cache = 0;
+        int max = 1;
+        int prev = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > prev) size++;
+            else {
+                int selfSplit = size >> 1;
+                int adjacentPair = Math.min(cache, size);
+                if (selfSplit > max) max = selfSplit;
+                if (adjacentPair > max) max = adjacentPair;
+                cache = size;
+                size = 1;
+            }
+            prev = num;
+        }
+        int selfSplit = size >> 1;
+        int adjacentPair = Math.min(cache, size);
+        if (selfSplit > max) max = selfSplit;
+        if (adjacentPair > max) max = adjacentPair;
+        return max;
+    }
+}
