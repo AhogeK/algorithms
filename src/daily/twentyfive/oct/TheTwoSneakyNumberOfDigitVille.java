4d33418ca10beebe0ac3230d305313730034db7d
AhogeK <ahogek@gmail.com>
2025-10-31 02:22:30 +0800

    feat(daily): add solution for sneaky numbers problem
    - Implemented algorithm to find two duplicate numbers in array
    - Used boolean array to track seen numbers efficiently
    - Optimized early return when both duplicates found
    - Added proper documentation and author information
    - Follows O(n) time complexity and O(1) space complexity

---
+++ src/daily/twentyfive/oct/TheTwoSneakyNumberOfDigitVille.java
@@ -0,0 +1,24 @@
+package daily.twentyfive.oct;
+
+/**
+ * 3289. 数字小镇中的捣蛋鬼
+ *
+ * @author AhogeK [ahogek@gmail.com]
+ * @since 2025-10-31 02:20:21
+ */
+public class TheTwoSneakyNumberOfDigitVille {
+    public int[] getSneakyNumbers(int[] nums) {
+        boolean[] seen = new boolean[100];
+        int[] ans = new int[2];
+        int k = 0;
+        for (int x : nums) {
+            if (seen[x])
+                ans[k++] = x;
+            else
+                seen[x] = true;
+            if (k == 2)
+                return ans;
+        }
+        return ans;
+    }
+}
