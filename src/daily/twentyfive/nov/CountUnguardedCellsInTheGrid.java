3234d3053a2a205280e9847d71bfb6cd3e084038
AhogeK <ahogek@gmail.com>
2025-11-02 19:13:06 +0800

    feat(grid): add solution for counting unguarded cells in grid
    - Implement grid traversal to mark guarded cells
    - Add logic to handle guards and walls positioning
    - Create directional scanning for guard vision
    - Count unguarded cells after processing
    - Include boundary checks during scanning
    - Return total count of unguarded cells

---
+++ src/daily/twentyfive/nov/CountUnguardedCellsInTheGrid.java
@@ -0,0 +1,36 @@
+package daily.twentyfive.nov;
+
+/**
+ * 2257. 统计网格图中没有被保卫的格子数
+ *
+ * @author AhogeK [ahogek@gmail.com]
+ * @since 2025-11-02 19:04:14
+ */
+public class CountUnguardedCellsInTheGrid {
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+        int[][] grid = new int[m][n];
+        for (int[] g : guards)
+            grid[g[0]][g[1]] = 2;
+        for (int[] w : walls)
+            grid[w[0]][w[1]] = 3;
+        int[][] dirs = {{-1,0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int[] g : guards) {
+            for (int[] d : dirs) {
+                int x = g[0] + d[0];
+                int y = g[1] + d[1];
+                while (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] < 2) {
+                    grid[x][y] = 1;
+                    x += d[0];
+                    y += d[1];
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) count++;
+            }
+        }
+        return count;
+    }
+}
