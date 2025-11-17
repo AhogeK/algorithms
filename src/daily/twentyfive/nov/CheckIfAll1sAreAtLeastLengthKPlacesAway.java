e730871ccc2ca55c58ccb2920289eef4ac8ce05b
AhogeK <ahogek@gmail.com>
2025-11-17 16:05:10 +0800

    feat(array): add solution for checking if all 1s are at least k places away
    - Implement KLengthApart method to verify distance between1s in binary array
    - Track previous 1 position and compare with current1 position
    - Return false immediately when distance constraint is violated
    - Handle edge case for first occurrence of 1
    - Add complete solution with proper variable initialization

---
+++ src/daily/twentyfive/nov/CheckIfAll1sAreAtLeastLengthKPlacesAway.java
@@ -0,0 +1,24 @@
+package daily.twentyfive.nov;
+
+/**
+ * 1437. 是否所有 1 都至少相隔 k 个元素
+ *
+ * @author AhogeK [ahogek@gmail.com]
+ * @since 2025-11-17 16:02:48
+ */
+public class CheckIfAll1sAreAtLeastLengthKPlacesAway {
+    public boolean KLengthApart(int[] nums, int k) {
+        int prev = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (prev == -1) {
+                    prev = i;
+                } else {
+                    if (i - prev < k + 1) return false;
+                    prev = i;
+                }
+            }
+        }
+        return true;
+    }
+}
