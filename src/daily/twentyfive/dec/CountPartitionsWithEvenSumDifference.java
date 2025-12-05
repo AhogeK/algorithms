9ca36ef9bf058a0bd2439838406883dd0eeb0ade
AhogeK <ahogek@gmail.com>
2025-12-05 10:59:48 +0800

    feat(algorithm): add solution for counting partitions with even sum difference
    - Implement core logic to calculate total sum of array elements
    - Add condition to check if total sum is even or odd
    - Return length minus one when sum is even
    - Return zero when sum is odd
    - Include complete class structure with documentation
    - Add author information and creation timestamp

---
+++ src/daily/twentyfive/dec/CountPartitionsWithEvenSumDifference.java
@@ -0,0 +1,18 @@
+package daily.twentyfive.dec;
+
+/**
+ * 3432. 统计元素和差值为偶数的分区方案
+ *
+ * @author AhogeK [ahogek@gmail.com]
+ * @since 2025-12-05 10:57:39
+ */
+public class CountPartitionsWithEvenSumDifference {
+    public int countPartitions(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums)
+            totalSum += num;
+        if (totalSum % 2 == 0)
+            return nums.length - 1;
+        else return 0;
+    }
+}
