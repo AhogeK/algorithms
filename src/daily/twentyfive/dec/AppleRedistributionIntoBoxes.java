acafaeec06905b992870aa964ac4c1e48355be9a
AhogeK <ahogek@gmail.com>
2025-12-24 10:42:47 +0800

    feat(daily): add apple redistribution into boxes solution
    - Implemented minimumBoxes method to calculate minimum boxes needed
    - Added logic to count total apples and box capacities
    - Created counting array for box capacity distribution
    - Implemented greedy algorithm to select boxes with highest capacity
    - Added loop to distribute apples using largest available boxes first
    - Returns minimum number of boxes required for redistribution

---
+++ src/daily/twentyfive/dec/AppleRedistributionIntoBoxes.java
@@ -0,0 +1,32 @@
+package daily.twentyfive.dec;
+
+/**
+ * 3074. 重新分装苹果
+ *
+ * @author AhogeK [ahogek@gmail.com]
+ * @since 2025-12-24 06:18:11
+ */
+public class AppleRedistributionIntoBoxes {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int total = 0;
+        for (int a : apple)
+            total += a;
+
+        int[] cnt = new int[51];
+        for (int c : capacity)
+            cnt[c]++;
+
+        int used = 0;
+        int sum = 0;
+
+        for (int cap = 50; cap >= 1 && sum < total; cap--) {
+            while (cnt[cap] > 0 && sum < total) {
+                sum += cap;
+                cnt[cap]--;
+                used++;
+            }
+        }
+
+        return used;
+    }
+}
